7aa8b102a44ee7b86b9fc197bed234f8d3ef214d
João Carlos Valadares Ribeiro Filho <joaocvr@dataprev.gov.br>
2021-10-26 14:16:20 -0300

    Set header cors configurations.

--- main/main.go
+++ main/main.go
@@ -12,10 +12,26 @@ func main() {
 	a := endpoint.Api{}
 
 	router := gin.Default()
-	router.GET("/news", a.GetNews)
+	router.Use(CORSMiddleware())
+	router.GET("/v1/news", a.GetNews)
 
 	err := router.Run("localhost:8080")
 	if err != nil {
 		fmt.Print(err.Error())
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
